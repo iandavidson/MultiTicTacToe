f6bfd2f9de116cb74e7d0e987f9653eefa2fa764
IanDavidson <IanDavidson>
2018-07-14 10:44:50 -0700

    initial commit

---
+++ frontEnd/TicTacToes.js
@@ -0,0 +1,568 @@
+// response object:
+//some information: AssignedMove default => -1
+var GameState = {//hardcoded response object
+  "gameId": 0,
+  "tictactoes": [
+      {
+        Position: 0,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            }
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 1,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 2,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 3,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 4,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 5,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 6,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 7,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+      {
+        Position: 8,
+        "tictactoe": [
+            {
+              "Position": 0,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 1,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 2,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 3,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 4,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 5,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 6,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 7,
+              "Value": "",
+              "AssignedMove": -1
+            },
+            {
+              "Position": 8,
+              "Value": "",
+              "AssignedMove": -1
+            },
+        ],
+        "State": "closed",
+        "Winner": "",
+        "Active": false
+      },
+  ],
+  "gameState": "open",
+  "winner": "",
+  "Player": 1
+};
+
+//Request object:
+var MoveRequest = {
+//"newGame": false,
+  "gameId": 0,
+  "player": "x",
+  "move": {
+    "tictactoe": null, //0-8
+    "cell": null //0-8
+  }
+};
+
+
+//NewGame button usage:
+
+//called when NewGame button is pressed in game
+//when clicked:
+  //submit similar post request (for now)
+  //generate new game ID
+  //return empty board
+  //re initialize() => could potentially be initialize or another similar function which does less
+        //=> side note: maybe change initalize() so it could support both (provide parameter to dx/dy)
+
+//current Information must be tracked
+var currentMove;
+var gameId;
+var gameOver; //true || false
+
+//intitialize function
+  //initialize some current information
+function initialize(){
+  /*
+    we must:
+      1. work thru tables and add classes, so we can later identify with $(#tableId .cellNumber)
+      2. use the response & apply that on the board
+
+  */
+
+  //works it way throught the tables adds clases to later be identifiable
+  var tableCellsTemp; //will be a container of html elements of type td
+  for(var i = 0; i < 9 ; i++){
+    tableCellsTemp = $("#table" + i.toString() + " tr td");
+    tableCellsTemp.addClass("Table" + i.toString() + "Cell")
+    //for(var j = 0; j < 9; j++){
+        //console.log("Cell" + j.toString() + "Table" + i.toString());
+        // tableCellsTemp[j].addClass( "Cell" + j.toString() + "Table" + i.toString() );
+        //console.log(tableCellsTemp[j]);
+    //}
+  }
+
+  // var tableCellsTemp = $("#table0").find("tr td");
+  // tableCellsTemp[0].addClass = "x";
+
+}
+
+
+function applyGameState(){
+
+}
+
+
+//test code to ensure jquery + html file interaction is correctly working
+// $("#H1Span").on("click", function (){
+//     alert(this.innerText);
+// });
+//
+// function clickTest(){
+//   alert("Anchor call worked");
+// }
+
+
+// $("table td").on("click", function(){
+//   //identifies when a specific "table Data" cell is clicked on,
+//   //this refers to the specific html element which fired the listener
+//     if(this.innerText == ""){
+//
+//     //let the user actually make a move
+//
+//
+//     }
+// });
